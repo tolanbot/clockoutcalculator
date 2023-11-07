3c6423f97cbfe255aaf9d712f3960998c66e903c
Chris Tolan <69782006+tolanbot@users.noreply.github.com>
2023-11-07 12:49:40 -0800

    Update handleForm.js
    Fix typo

--- handleForm.js
+++ handleForm.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const zeroRegex = /^[0]{1,2}:[0]{1,2}\s*[APap][Mm]$/
         const regex = /^[0-9]{1,2}:[0-5][0-9]\s*[APap][Mm]$/;
         try{
-            if(zeroRegex.test(str1) || zeroRegex.test(str2) || zeroRefex.test(str3) || !regex.test(str1) || !regex.test(str2) || !regex.test(str3)){
+            if(zeroRegex.test(str1) || zeroRegex.test(str2) || zeroRegex.test(str3) || !regex.test(str1) || !regex.test(str2) || !regex.test(str3)){
                 alert("TIME FORMAT SHOULD BE 'h:mm' ENDING IN 'AM/am' or 'PM/pm'")
                 throw new Error()
             }
